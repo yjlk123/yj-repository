e7383cd05953354e9914899a73425f5a3d6b7626
yjlk123 <yjlk123@163.com>
2018-03-12 22:01:57 +0800

    My first Web Project

---
+++ Project1/script/global.js
@@ -0,0 +1,83 @@
+/*
+* @Author: yj
+* @Date:   2018-02-15 09:35:59
+* @Last Modified by:   yj
+* @Last Modified time: 2018-03-11 11:47:10
+*/
+/**
+页面公共的函数放这个文件里
+*/
+
+/*用于文档加载后启动函数*/
+function addLoadEvent(func){
+	var oldonload = window.onload;
+	if (typeof window.onload != 'function'){
+		window.onload = func;
+	}
+	else{
+		window.onload = function(){
+			oldonload();
+			func();
+		}
+	}
+}
+
+/*将新的元素添加到指定元素的后面*/
+function insertAfter(newElament, targetElement){
+	var parent = targetElement.parentNode;
+	if(parent.lastChild == targetElement){
+		parent.appendChild(newElament);
+	}
+	else{
+		parent.insertBefore(newElament,targetElement.nextSibling);
+	}
+}
+
+/*为元素添加类*/
+function addClass(element, value){
+	if(!element.className){
+		element.className = value;
+	}
+	else{
+		newClassName = element.className;/*不需要设置变量?还是newClassName是个全局变量？*/
+		newClassName+= "";
+		newClassName+= value;
+		element.className = newClassName;/*这个也需要手动更新*/
+	}
+}
+
+/*到当前页就凸显导航栏*/
+function heightlight(){
+	if(!document.getElementsByTagName){/*添加检查是否支持该函数*/
+		return false;
+	}
+	if(!document.getElementById){/*注意不是getElementsById,没有s*//*添加检查是否支持该函数*/
+		return false;
+	}
+	var navs = document.getElementsByTagName('nav');/*标签都是document对象的，所以用document来查找nav*/
+	if(navs.length == 0){
+		return false;
+	}
+	var links = navs[0].getElementsByTagName("a");
+	var linkurl;
+	var linkurltag = "recommendtag.html";
+	var	linkurldaren = "recommenddaren.html";
+	for(var i=0; i<links.length; i++){/*获得导航链接后循环它们*/
+		linkurl = links[i].getAttribute("href");
+		if(window.location.href.indexOf(linkurl)!= -1){
+			links[i].className = "here";/*为a添加类*/
+			var linktext = links[i].lastChild.nodeValue.toLowerCase();
+			document.body.setAttribute("id", linktext);/*为该页的body添加id:链接的文本内容*/
+		}
+	}
+	
+	/*因为导航栏每个页面重写一次的原因而产生的问题，这个解决办法不好*/
+	if(window.location.href.indexOf(linkurltag)!= -1){
+			links[1].className = "here";/*为a添加类*/
+	}
+	else if(window.location.href.indexOf(linkurldaren)!= -1){
+			links[1].className = "here";/*为a添加类*/
+	}
+}
+
+addLoadEvent(heightlight);
---
+++ Project1/script/recommend.js
@@ -0,0 +1,130 @@
+/*
+* @Author: yj
+* @Date:   2018-03-07 10:14:35
+* @Last Modified by:   yj
+* @Last Modified time: 2018-03-12 08:50:55
+*/
+/**
+recommend.thml对应的js文件
+*/
+
+function $(id) {
+    //判断id的类型
+    return typeof id === 'string'?document.getElementById(id):id;////////////////////////////////
+}
+
+//当网页加载完毕
+function createBox() {
+    //瀑布流布局 保证传的参数能够找到父盒子
+    waterFall('main','box');
+    
+    //滚动加载盒子
+    window.onscroll = function ()///////////?进不去这个函数，貌似和css样式的100%有关
+    {
+    	alert("1");
+        //判断是否加载
+        if (checkWillLoad())
+        {
+            //创造假数据
+            var data = {'dataImg':[{'img':'23.jpg'},{'img':'24.jpg'},{'img':'25.jpg'},{'img':'26.jpg'},{'img':'27.jpg'},{'img':'28.jpg'}]};
+            //加载数据
+            for(var i=0; i<data.dataImg.length; i++)
+            {
+                //创建最外面的盒子
+                var newBox = document.createElement('div');
+                newBox.className = 'box';
+                $('main').appendChild(newBox);
+                //创建单个盒子
+                var newPic = document.createElement('div');
+                newPic.className = 'pic';
+                newBox.appendChild(newPic);
+                //创建img
+                var newImg = document.createElement('img');
+                newImg.src = 'images/' + data.dataImg[i].img;
+                newPic.appendChild(newImg);
+            }
+            //把刚创建的盒子瀑布流布局
+            waterFall('main','box');
+        }
+    }
+}
+
+//网页加载后调用函数
+addLoadEvent(createBox);
+
+//实现瀑布流布局
+//规则:从第二行开始的图片,总是拼接在上一行高度最矮的图片后面
+function  waterFall(parent,box) {
+    //父盒子居中
+    //通过父盒子拿到所有的子盒子
+    var allBox = $(parent).getElementsByClassName(box);
+    //求出盒子的宽度
+    var boxWidth = allBox[0].offsetWidth;
+    //求出浏览器的宽度(包括边框的宽高)
+    var screenWidtn = document.body.offsetWidth;
+    //求出列数 //取整函数取整
+    var cols = Math.floor( screenWidtn/boxWidth);
+    //父标签居中
+    //先求出父标签宽度
+    $(parent).style.width = boxWidth * cols + 'px';
+    //居中
+    $(parent).style.margin = '0 auto';
+    
+    //子盒子定位
+    //创建一个高度数组,存所有的高度
+    var heightArr = [];
+    //遍历
+    for(var i = 0; i < allBox.length ;i++)
+    {
+        //求出每个盒子的高度
+        var boxHeight = allBox[i].offsetHeight;
+        //第一行的盒子不需要重新定位//每一行的盒子数与列数相同
+        if(i<cols)
+        {
+            //添加第一行所有盒子高度
+            heightArr.push(boxHeight);
+        }
+        else//剩下的盒子都需要瀑布流布局
+        {
+            //求出最矮的盒子高度
+            var minBoxHeight = Math.min.apply(this,heightArr);
+            //求出最矮盒子对应的索引
+            var minBoxIndex = getMinBoxIndex(minBoxHeight,heightArr);
+            //盒子瀑布流定位  顶部间距就是最矮盒子的高度
+            allBox[i].style.position = 'absolute';
+            allBox[i].style.top = minBoxHeight + 'px';
+            allBox[i].style.left = minBoxIndex * boxWidth +'px';
+            //关键:更新数组最矮高度,使下一个图片在高度数组中总是找最矮高度的图片下面拼接
+            heightArr[minBoxIndex] += boxHeight;
+        }
+    }
+}
+
+//求出最矮盒子对应的索引函数
+function getMinBoxIndex(val,arr) {
+    for(var i in arr)
+    {
+        if(val == arr[i])
+        {
+            return i;
+        }
+    }
+}
+
+//加载更多规则:当浏览器最下方到达图片的高度一半时让其刷新出来
+//判断是否符合加载条件
+function checkWillLoad() {
+    //取出所有盒子
+    var allBox = $('main').getElementsByClassName('box');
+    //取出最后一个盒子
+    var lastBox = allBox[allBox.length - 1];
+    //求出最后一个盒子高度的一半 + 内容与浏览器头部的偏离位置
+    var lastBoxDis = lastBox.offsetHeight * 0.5 + lastBox.offsetTop;
+    //求出浏览器的高度
+    // 注意:JS代码存在浏览器兼容问题 一般分标准模式(按屏幕算document.body.offsetHeight)和混杂模式(按所有内容算)
+    var screenHeight =  document.documentElement.clientHeight;
+    //页面偏离屏幕的高度
+    var scrollTopHeight = document.body.scrollTop;
+    //判断
+    return lastBoxDis <= screenHeight + scrollTopHeight;
+}
