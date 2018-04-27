e69032c56b97de2aca36c379e767215eff9ec47b
yjlk123 <yjlk123@163.com>
2018-04-27 10:39:54 +0800

    test.js修改，用于测试javascript的知识点

--- Project1/script/test.js
+++ Project1/script/test.js
@@ -2,7 +2,7 @@
 * @Author: yj
 * @Date:   2018-04-05 20:29:14
 * @Last Modified by:   yjlk123
-* @Last Modified time: 2018-04-06 10:55:13
+* @Last Modified time: 2018-04-26 20:56:29
 */
 function Person(sum){
 	var namesum = sum+1;
@@ -17,5 +17,70 @@ Person.prototype.getInfo = function(){
 var will = new Person(0);
 will.getInfo();
 
-var wi = new Person(7);
-wi.getInfo();
+
+console.log('**************************');
+//tenth
+var test ={
+  'a':1,
+  'b':function(){
+   console.log(this === test);//这里不论是test还是test1,都是true
+  }
+ }
+var test1 = test;
+test1.b();//true   注意这个，都指向同一个对象，默认为二者是同一个，所以this===test输出结果是true
+//你暂且理解为引用的了，两个都指向一个对象
+//你不会认为结果为"false"吧，错了，虽然'test1'的值为'test'  但是'test1'不还是'test'对象么，
+//它有新产生对象，你暂且理解为引用的了，两个都指向一个对象，奉上下面一段代码为证
+
+var test2 ={
+  'a':1,
+  'b':function(){
+   console.log(this === test2)//这里不论是test2还是test1,都是true
+  }
+ }
+var test1 = test2;
+test2.a = 2;
+console.log(test1.a);//2
+test1.b();//true
+
+
+
+
+var test3 ={
+  'a':1,
+  'b':{
+   'b1':function(){
+    console.log(this === test3);//若改成test3.b，则结果是true
+   }
+  }
+ }
+test3.b.b1();//false    
+//按照上面的理论，这时"this"不再直接被'test'调用了，而是被'test.b' 调用, 奉上下面一段代码为证
+
+
+var test5 = function(){
+  var innerTest = function(){
+   console.log(this === test5);
+  }
+  innerTest();
+ }
+test5();
+
+//你不会认为弹出"true"吧，不是按照上面的理论'innerTest'是被'test5'调用的，然后'this'就指向'test5'吗？
+//额，错就错在是谁调用的'innerTest', 其实这种函数都是'window'对象调用的，(注意和上一段代码区别,上面的代码是对象，这里是函数，不一样)
+//即使你嵌套一千层，调用各个函数的都是'window'对象,奉上下面这段代码为证
+
+
+//注意，带有window关键字的会报错，不知道是哪里没有调好，所以注释掉了
+// var test6 = function(){
+//   var innerTest = function(){
+//    console.log(this === window);
+//    var innerTest1 = function(){
+//     console.log(this === window);
+//    }
+//    innerTest1();
+//   }
+//   innerTest();
+//  }
+// test6();
+
--- Project1/testjs.js
+++ Project1/testjs.js
@@ -1,20 +1,189 @@
-console.log("hello world");
 
+//first
 function Person(){
 	this.i = 100;
 }
 
 var i = 10;////////////////注意，这里加var与不加的结果并不相同
-Person();
-console.log(i);
+Person();//相当于一个全局的， i=90; 但不能和下面那种情况看作一样，因为这个全局变量的建立是在函数内部
+console.log(i);//是var i = 10 这个
+// 1.都加var，在方法内则是局部变量，在方法外则是全局变量。
+// 2.在方法内,加var为局部变量，不加var则是全局变量（在执行当前方法之后）
 
 
+
+//seventh  //////////////////为啥和first的结果不一样呢？？？？？？
+console.log('seventh');
+var b=10;
+b=60;//这里并不是添加全局的变量了，而是将b的值更改为60，原因是这样不加var的情况下，
+//首先会在自己的作用域搜索有没有已经定义了的，有的话就更改，没有的话就是为全局添加一个可以删除的变量
+//注意：若交换上面2句的位置，结果不一样，因为这就是两种定义全局变量的方式了。所以建议定义全局变量时加var
+console.log(b); //60 输出上一个值不会输出undefined
+
+
+var var01 = 1;
+function funtest() {
+ var01 = 0;
+ console.log(var01);
+ 
+}
+funtest();
+
+
+
+//third  /////////////////////为啥与前2个又不一样？
+var a = 'hello World';
+function bb(){
+    var a = 'Bill';
+    console.log(a);   
+}
+bb()   // ' Bill'
+console.log(a);    // 'hello world'
+
+
+var e = 'hello world';
+function cc(){
+    e = 'Bill';
+    console.log(e);
+}
+cc();   // 'Bill'  虽然定义的一个全局变量，但因为在函数中定义的，所以可以访问它
+console.log(e);     // 'Bill'  注意这个的输出
+
+
+
+//ninth
 var x = 2;
 　　function test(){
 　　　　this.x = 1;
 　　}
-　　var o = new test();
+var o = new test();
 console.log(x);
+
+
+var01 = 1;
+function funtest() {
+ console.log(var01);
+ var01 = 0;
+}
+
+
+//second
+num = 9; 
+var mymodule = {
+  num: 81,
+  getNum: function() { console.log(this.num);}
+};
+
+mymodule.getNum(); // 81
+
+var getNumy = mymodule.getNum;
+getNumy(); // 9, 因为在这个例子中，"this"指向全局对象
+
+// 创建一个'this'绑定到module的函数
+var boundGetNum = getNumy.bind(mymodule);
+boundGetNum(); // 81
+
+
+
+
+
+
+//fourth
+function hh(){
+    console.log(al);
+    var al = 'hello world';
+}
+hh();    //undefined   //注意：若上下2个函数同名，则会执行下面这个函数，因为js里的函数没有重写一说
+
+
+function hb(){
+    //console.log(bl);
+    bl = 'hello world';
+}
+//hb();    // 报错：'bl is not defined' ，就算注释掉这句，不调用，也会报错，因为编译就发现的错误
+
+
+//fifth
+var num1 = 1; 
+ num2 = 2; 
+ //delete num1;  //无法删除，报错
+ // delete num2;  删除 
+ function model(){ 
+        var num1 = 7; // 本地变量 
+        num2 = 6;     // window的属性 
+        // 匿名函数 
+        (function(){ 
+               var num = 5; // 本地变量 
+               num1 = 9; // 继承作用域（闭包）
+               console.log(num1); 
+               num3 = 3; // window的属性 
+        }())
+ } 
+
+model();
+console.log(num1);
+
+
+ //sixth
+ //这段报错，测试时可暂时删掉
+ /*
+ fod();       //TypeError: foo is not a function 输出foo is not a function 原因是：js解析遇到 foo()时会默认当做函数来解析
+    var fod = function(){
+        console.log("aaa");
+    }
+*/
+
+
+ console.log(foo);
+    var foo=10;
+    console.log(foo);
+    function foo(){
+        console.log(20);
+    }
+    console.log(foo);
+
+//上面这段变量提升和函数提升相当于：
+// function foo(){
+//         console.log(10);
+//     }
+// var foo;
+// console.log(foo);
+// foo=10;
+// console.log(foo);
+// console.log(foo);
+
+
+//注意： 函数提升在变量提升上面，第一个console.log(foo);
+//为什么会输出函数题呢，原因在于 var foo; 并未有赋值只是声明，因此他会调用上面的值
+
+
+
+
+// //eighth  关于bind的用法
+// var altwrite = document.write;
+// altwrite("hello");
+// altwrite.bind(document)("hello");
+// //这里为什么出现非法调用？illegal invocation
+
+
+
+
+
+
+//原型相关///////////////////////////////////////////////////?
+var test = function(){
+ }
+ var my = function(){
+  this.a = function(){
+  console.log(this === mytest2);
+  }
+ }
+ var mytest = new my();
+ test.prototype = mytest;
+ var mytest2 = new test();
+ mytest2.a();
+
+
 
 
 
