91e673af415fa28317051c79d03fca96d853cbe3
yjlk123 <yjlk123@163.com>
2018-05-10 18:46:13 +0800

    部分图片修改

--- Project1/script/recommend.js
+++ Project1/script/recommend.js
@@ -2,7 +2,7 @@
 * @Author: yj
 * @Date:   2018-03-07 10:14:35
 * @Last Modified by:   yj
-* @Last Modified time: 2018-05-10 18:22:56
+* @Last Modified time: 2018-05-10 18:36:17
 */
 /**
 recommend.thml对应的js文件
@@ -204,10 +204,11 @@ function createBox() {
             { 'usernamer':'Food tester','usericons':'204.jpg','img':'5.jpg','content':'铲屎官，你的地盘被朕征用啦！'},
             { 'usernamer':'Food tester','usericons':'205.jpg','img':'16.jpg','content':'你是我的花呀'},
             { 'usernamer':'Food tester','usericons':'205.jpg','img':'14.jpg','content':'立夏 | 竹杖芒鞋轻胜马，一蓑烟雨任平生。'},           
-            { 'usernamer':'Food tester','usericons':'205.jpg','img':'6.jpg','content':'午后与puppy'},
+            { 'usernamer':'Food tester','usericons':'205.jpg','img':'6.jpg','content':'卷卷真的好喜欢活力满满的运动场呀～手臂上的皮炎也好啦，开心！！！'},
             { 'usernamer':'Food tester','usericons':'205.jpg','img':'13.jpg','content':'午后与puppy'},
-            { 'usernamer':'Food tester','usericons':'205.jpg','img':'17.jpg','content':'卷卷真的好喜欢活力满满的运动场呀～手臂上的皮炎也好啦，开心！！！'},
+            { 'usernamer':'Food tester','usericons':'205.jpg','img':'17.jpg','content':'我们要懂得一天一天明白自己的平凡，同时也要一天一天愈加相信自己有点不平凡！'},
             { 'usernamer':'Food tester','usericons':'205.jpg','img':'15.jpg','content':'好吧暖光系画手根本不会冷下来.....'},
+            { 'usernamer':'Food tester','usericons':'205.jpg','img':'18.jpg','content':'小迪门岛是丹麦法罗群岛18个岛屿中最小的一座，但是它经常有自己独特的云。'},
             { 'usernamer':'Food tester','usericons':'206.jpg','img':'7.jpg','content':'没错，我就是吃货,铲屎官，你的地盘被朕征用啦！/////////////////////////'}]};
             //加载数据，先把盒子全建好顺序添加，添完全部的之后再计算布局
             for(var i=0; i<data.dataImg.length; i++)
